7a980153e26e6ade2bda08bc23543825096fc2ea
dev <dev@RIVNE.ITSTEP>
2023-06-07 11:48:59 +0300

    New commit

---
+++ ifELse.py
@@ -0,0 +1,73 @@
+# import random
+
+# number=random(int(1,5))
+# #number=input("Enter day number: ")
+# if number == "1":
+#     print("monday")
+# elif number == "2":
+#     print("Tuesday")
+# else:
+#     print("Error")
+
+
+#1
+# number=int(input("Enter first number: "))
+# number1=int(input("Enter second number: "))
+# result=number-number1
+
+# print("Sum: ", number+number1)
+# print("Difference: ", result)
+# print("Dobutok: ", number*number1)
+
+#2
+# number=int(input("Enter first number: "))
+# number1=int(input("Enter second number: "))
+
+# if number>number1:
+#     print("Max number: ", number)
+# elif number<number1:
+#     print("Max number: ", number1)
+
+#3
+# number=int(input("Enter first number: "))
+# number1=int(input("Enter second number: "))
+
+# if number<number1:
+#     print("Min number: ",number)
+# elif number1<number:
+#     print("Min number: ", number1)
+
+#4
+# number=int(input("Enter first number: "))
+# number1=int(input("Enter second number: "))
+
+# print("Now enter your choice:\n\ta)Sum;\tb)Difference;\tc)Dobutok;")
+
+# choice=input("Enter choice: ")
+
+# if choice=="a":
+#     print("Sum: ", number+number1)
+# elif choice=="b":
+#     if number>number1:
+#         print("Difference: ",number-number1)
+#     elif number1>number:
+#         print("Difference: ",number1-number)
+# elif choice=="c":
+#     print("Dobutok: ", number*number1)
+
+#5
+# line=input("Enter your line use \"*\": ")
+
+# if len(line)==1:
+#     print("*")
+# elif len(line)==2:
+#     print("**")
+# elif len(line)==3:
+#     print("***")
+# elif len(line)==4:
+#     print("****")
+
+#6
+number=int(input("Enter number: "))
+number1=input("Enter symbol: ")
+
