723795d3f0cdf3e33ea1c89fd19dafd92a370682
dev <dev@RIVNE.ITSTEP>
2023-06-12 11:56:55 +0300

    New commit

---
+++ Classwork/classwork.py
@@ -0,0 +1,59 @@
+'''number = int(input("Enter number: "))
+
+if number % 2 == 0:
+    print(f"Even number: {number}")
+else:
+    print(f"Odd number: {number}")'''
+
+
+'''number = int(input("Enter number: "))
+
+if number % 7==0:
+    print("Number is a multiple of 7")
+else:
+    print("Number is not a multiple of 7")'''
+
+'''num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1>num2:
+    print(f"Larger {num1}")
+elif num2>num1:
+    print(f"Larger: {num2}")'''
+
+'''num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1<num2:
+    print(f"Smalest: {num1}")
+elif num2<num1:
+    print(f"Smalest: {num2}")'''
+
+'''num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+choice = int(input("What u want: \n1)Sum;\n2)Difference;\n3)Average;"))
+
+if choice == 1:
+    print(f"Sum: {num1+num2}")
+elif choice == 2:
+    print(f"Difference: {num1 - num2}")
+elif choice == 3:
+    print(f"Average: {(num1+num2)/2}")'''
+
+num1 = int(input("Enter first number: "))
+
+if num1==1:
+    print("Monday")
+elif num1==2:
+    print("Thusday")
+elif num1 ==3:
+    print("wednsday")
+elif num1==4:
+    print("thurthday")
+elif num1 ==5:
+    print("Friday")
+elif num1 ==6:
+    print("Suterday")
+elif num1 ==7:
+    print("Sunday")
--- My.py
+++
@@ -1,3 +0,0 @@
-print("Nothing\nwill work\nunless you do\ncl")
-
-print("\"Anyone who\nstops\nlearning is\nold,\nwhether at\ntwenty or\neight\"\nHenry Ford")
